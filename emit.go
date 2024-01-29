07a7e3a1c66913960f062a050cce1cf2498cbd5e
Jonas Hiltl <jonashiltl2003@gmail.com>
2024-01-29 22:18:14 +0100

    add testify & use chanx batcher impl

--- emit.go
+++ emit.go
@@ -52,19 +52,19 @@ func Emit[T any](e *Emitter, v T) error {
 }
 
 type OnOptions struct {
-	maxTimeout   time.Duration
-	maxBatchSize int
+	timeout   time.Duration
+	batchSize int
 }
 
 func WithTimeout(maxTimeout time.Duration) func(*OnOptions) {
 	return func(s *OnOptions) {
-		s.maxTimeout = maxTimeout
+		s.timeout = maxTimeout
 	}
 }
 
 func WithBatchSize(maxBatchSize int) func(*OnOptions) {
 	return func(s *OnOptions) {
-		s.maxBatchSize = maxBatchSize
+		s.batchSize = maxBatchSize
 	}
 }
 
@@ -83,39 +83,71 @@ func On[T any](e *Emitter, fn func([]T), options ...func(*OnOptions)) {
 		e.subs[key[T]{}] = make([]chan any, 0, 3) // default capacity of 3
 	}
 
-	cn := make(chan any)
-	e.subs[key[T]{}] = append(e.subs[key[T]{}], cn)
+	ch := make(chan any)
+	e.subs[key[T]{}] = append(e.subs[key[T]{}], ch)
 
-	go func() {
-		for {
-			var batch []T
+	go batch(ch, opts.batchSize, opts.timeout, fn)
+}
 
-			var expire <-chan time.Time
-			if opts.maxTimeout > 0 {
-				expire = time.After(opts.maxTimeout)
+// batch reads from a channel and calls fn with a slice of batchSize.
+// copied from https://github.com/smallnest/exp/blob/master/chanx/batcher.go
+func batch[T any](ch <-chan any, batchSize int, timeout time.Duration, fn func([]T)) {
+	var expire <-chan time.Time
+	if timeout > 0 {
+		expire = time.After(timeout)
+	}
+
+	for batchSize <= 1 { // sanity check,
+		for v := range ch {
+			if v, ok := v.(T); ok {
+				fn([]T{v})
 			}
+		}
 
-			for {
-				select {
-				case value, ok := <-cn:
-					if !ok {
-						goto done
-					}
+		return
+	}
 
-					if v, ok := value.(T); ok {
-						batch = append(batch, v)
-					}
-					if opts.maxBatchSize == 0 || len(batch) == opts.maxBatchSize {
-						goto done
-					}
-				case <-expire:
-					goto done
-				}
-			}
-		done:
+	// batchSize > 1
+	var batch = make([]T, 0, batchSize)
+	for {
+		select {
+		case <-expire:
 			if len(batch) > 0 {
 				fn(batch)
 			}
+			return
+		case v, ok := <-ch:
+			if !ok { // closed
+				fn(batch)
+				return
+			}
+			if v, ok := v.(T); ok {
+				batch = append(batch, v)
+			}
+			if len(batch) == batchSize { // full
+				fn(batch)
+				batch = make([]T, 0, batchSize) // reset
+			}
+		default:
+			if len(batch) > 0 { // partial
+				fn(batch)
+				batch = make([]T, 0, batchSize) // reset
+			} else { // empty
+				// wait for more
+				select {
+				case <-expire:
+					return
+				case v, ok := <-ch:
+					if !ok {
+						return
+					}
+
+					if v, ok := v.(T); ok {
+						batch = append(batch, v)
+					}
+				}
+
+			}
 		}
-	}()
+	}
 }
--- emit_test.go
+++ emit_test.go
@@ -1,20 +1,58 @@
 package emit
 
 import (
-	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRegisterSub(t *testing.T) {
 	type event string
 
 	e := &Emitter{}
-	On(e, func(t []event) {
-		fmt.Println(t)
-	}, WithBatchSize(10), WithTimeout(10*time.Second))
+	On(e, func(t []event) {}, WithBatchSize(10), WithTimeout(10*time.Second))
 
-	if len(e.subs) != 1 || len(e.subs[key[event]{}]) != 1 {
-		t.Fatal("Failed to register subscriber")
+	assert.Len(t, e.subs, 1)
+	assert.Len(t, e.subs[key[event]{}], 1)
+}
+
+func TestBatch(t *testing.T) {
+	ch := make(chan any, 10)
+	for i := 0; i < 10; i++ {
+		ch <- i
 	}
+
+	var count atomic.Int64
+	go batch[int](ch, 5, 0, func(batch []int) {
+		if len(batch) != 5 {
+			assert.Fail(t, "expected batch size 5, got %d", len(batch))
+		}
+		count.Add(int64(len(batch)))
+	})
+	time.Sleep(time.Second)
+	close(ch)
+	assert.Equal(t, int64(10), count.Load())
+
+	ch = make(chan any, 10)
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+
+	count.Store(0)
+	i := 0
+	go batch[int](ch, 3, 0, func(batch []int) {
+		if i < 3 && len(batch) != 3 {
+			assert.Fail(t, "expected batch size 5, got %d", len(batch))
+		}
+		if i == 3 && len(batch) != 1 {
+			assert.Fail(t, "expected batch size 1, got %d", len(batch))
+		}
+		i++
+		count.Add(int64(len(batch)))
+	})
+	time.Sleep(time.Second)
+	close(ch)
+	assert.Equal(t, int64(10), count.Load())
 }
